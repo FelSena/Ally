0051658f8520e8c6eb3a08238b20a3fa45e2542e
Felipe Sena <felipe.desena@hotmail.com>
2022-10-26 16:39:59 -0300

    first dependences

---
+++ teste_ally/src/components/UserForm/index.jsx
@@ -0,0 +1,12 @@
+const UserForm = () => {
+  return (
+    <form action="submit">
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+    </form>
+  );
+};
+
+export default UserForm;
